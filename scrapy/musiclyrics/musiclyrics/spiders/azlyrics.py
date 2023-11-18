aba17cc040f3d45b5ad27bc77bfa1a85fbd060b2
Michele Smaldone <michele.smaldone@usi.ch>
2023-11-18 11:57:39 +0100

    azlyrics crawler added.

--- scrapy/musiclyrics/musiclyrics/spiders/azlyrics.py
+++ scrapy/musiclyrics/musiclyrics/spiders/azlyrics.py
@@ -6,13 +6,28 @@ import json
 
 class azlyrics(scrapy.Spider):
     name = "azlyrics"
-    allowed_domains = ["https://www.azlyrics.com","www.azlyrics.com"]
+    allowed_domains = ["www.azlyrics.com"]
     # start_urls = list(["https://www.azlyrics.com/" + i + ".html" for i in ascii_lowercase])
     start_urls = ["https://www.azlyrics.com/a.html"]
 
+
+    def parse_artist_page(self,response):
+        for song_title in response.xpath("//div[@class='listalbum-item']/a"):
+            print(song_title.attrib["href"])
+            song_page = "https://" + self.allowed_domains[0] + song_title.attrib["href"]
+            yield scrapy.Request(song_page, callback=self.parse_get_data)
+
+    def parse_get_data(self,response):
+        return {
+            "title": response.xpath("//div[@class='col-xs-12 col-lg-8 text-center']/b[1]").get() ,
+            "artist": response.xpath("//div[@class='lyricsh']//b").get(),
+            "lyrics": response.xpath("//div[@class='col-xs-12 col-lg-8 text-center']/div[5]").get()
+        }
+
     def parse(self, response):
-        for col in response.xpath("html body.margin50 div.container.main-page div.row div.col-sm-6.text-center.artist-col"):
-            print(col.get())
+        for title in response.xpath("//div[@class='col-sm-6 text-center artist-col']/a"):
+            artist_page = "https://" + self.allowed_domains[0] + "/" + title.attrib["href"]
+            yield scrapy.Request(artist_page, callback=self.parse_artist_page)
 
 
 # /html/body/div[2]/div/div[2]
