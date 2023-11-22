06e4cc2f3c0331ebf6a95984947e5be196fd7131
Michele Smaldone <michele.smaldone@usi.ch>
2023-11-22 23:01:31 +0100

    v-autocomplete changed with v-combobox.

--- frontend2/search-engine/src/components/SearchEngineHomePage.vue
+++ frontend2/search-engine/src/components/SearchEngineHomePage.vue
@@ -1,21 +1,62 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      query: null,
+      q2: null,
+      song_list: []
+    }
+  },
+
+  methods: {
+    async fetch_songs_by_query() {
+      console.log('query: ', this.query);
+      if (this.query) {
+        const res = await fetch(`http://localhost:8080/search/lyrics/${this.query}`)
+        const songs = await res.json()
+        this.song_list = songs
+      }
+    }
+  },
+
+  watch: {
+
+  }
+
+
+
+
+}
 
 </script>
 
 <template>
-  <v-autocomplete
+  <v-combobox
       
       auto-select-first
       class="flex-full-width"
       density="comfortable"
       item-props
+      hide-no-data
       menu-icon=""
       placeholder="Search a lyric"
       prepend-inner-icon="mdi-magnify"
       rounded
+      v-model="query"
+
       theme="light"
       variant="solo"
-  ></v-autocomplete>
+  ></v-combobox>
+
+  <v-btn @click="fetch_songs_by_query()">
+    submit
+  </v-btn>
+
+
+    {{query}}
+  <input v-model="q2" />
+  {{q2}}
+
 </template>
 
 <style scoped>
