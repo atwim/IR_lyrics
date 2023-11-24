8aa7f1fcffeec450cb9ed9d4b5cd9f3056e0a82a
Michele Smaldone <michele.smaldone@usi.ch>
2023-11-24 16:07:45 +0100

    SearchEngineHomePage: added error handling, loading state,  for each for SongCard and some comments to know  what are the next steps.

--- frontend2/search-engine/src/components/SearchEngineHomePage.vue
+++ frontend2/search-engine/src/components/SearchEngineHomePage.vue
@@ -1,11 +1,18 @@
 <script>
 import SongCard from "@/components/SongCard.vue";
 
+
+// 1.1 check that props song is working (songs are sent to SongCard)
+// 1.2 check logic for v-row
+// 2. review loading (show a loading while the songs are taken from backend)
+// 3. error handling
+
 export default {
   components: {SongCard},
   data() {
     return {
       query: null,
+      loading: false,
       song_list: []
     }
   },
@@ -13,13 +20,31 @@ export default {
   methods: {
     async fetch_songs_by_query() {
       console.log('query: ', this.query);
-      if (this.query) {
-        const res = await fetch(`http://localhost:8080/search/lyrics/${this.query}`)
-        const songs = await res.json()
-        this.song_list = songs;
+      try {
+        if (this.query) {
+          // this loading is not used for now
+          this.loading=true
+          const res = await fetch(`http://localhost:8080/search/lyrics/${this.query}`)
+          const songs = await res.json()
+          this.song_list = songs;
+        }
+      } catch (err) {
+        // TODO: handle the error
+        console.log("error occured");
+        this.song_list = [];
+      } finally {
+        // this loading is not used for now
+        this.loading = false;
       }
+
     }
   },
+  watch: {
+    // function to debug the query, everytime query changes its console logged.
+    query: function(newQuery) {
+      console.log('query changed:', newQuery);
+    }
+  }
 }
 
 </script>
@@ -35,6 +60,7 @@ export default {
       menu-icon=""
       placeholder="Search a lyric"
       prepend-inner-icon="mdi-magnify"
+      clearable
       rounded
       v-model="query"
 
@@ -49,10 +75,11 @@ export default {
 
     print query: {{query}}
 
-    <SongCard v-for="song in song_list"
-                :song="song"
-                :key="song.id"></SongCard>
-
+    <v-row v-for="song in song_list"
+           :key="song.id">
+      <SongCard   :song="song">
+      </SongCard>
+    </v-row>
 </template>
 
 <style scoped>
