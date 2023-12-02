05af563f9c5493d6dfaa0d1604d2a69fa8a58742
atwim <atwim@usi.ch>
2023-12-02 21:47:40 +0100

    working on clustering

--- backend/indexing_lyrics.py
+++ backend/indexing_lyrics.py
@@ -1,6 +1,18 @@
 import pyterrier as pt
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import nltk
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+
+from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 if not pt.started():
     pt.init()
@@ -69,4 +81,58 @@ indexref1 = indexer.index(song_info, fields=RETRIEVAL_FIELDS)
 bm25 = pt.BatchRetrieve(indexref1, wmodel="BM25")
 
 
-print(retriever_song_title(bm25.search("time")))
+# print(retriever_song_title(bm25.search("time")))
+
+
+# clustering
+
+stemmer = PorterStemmer()
+def apply_stem(lyrics, stemmer):
+  words = word_tokenize(lyrics)
+  stemmed_text = ' '.join([stemmer.stem(word) for word in words])
+  return stemmed_text
+
+stemmed_lyrics = [apply_stem(str(lyrics), stemmer) for lyrics in songs_data.lyrics]
+
+
+vectorizer = TfidfVectorizer(stop_words='english', max_df = .9, min_df = 0.01)
+
+# vectorizer the text documents
+vectorized_documents = vectorizer.fit_transform(stemmed_lyrics)
+# print(vectorized_documents)
+# reduce the dimensionality of the data using PCA
+pca = PCA(n_components=3)
+reduced_data = pca.fit_transform(vectorized_documents.toarray())
+
+# cluster the documents using k-means
+num_clusters = 15
+kmeans = KMeans(n_clusters=num_clusters, random_state=0)
+kmeans.fit(vectorized_documents)
+
+# create a dataframe to store the results
+results = pd.DataFrame()
+results['document'] = songs_data.title
+results['cluster'] = kmeans.labels_
+print(len(results['cluster']))
+# stemmed_lyrics = pd.concat([stemmed_lyrics, pd.DataFrame([["this is a song about things"]])], ignore_index=True)
+# kmeans.fit(vectorizer.fit_transform(vectorized_documents.toarray()))
+# results['document'] = songs_data.title
+# results['cluster'] = kmeans.labels_
+# print(len(results['cluster']))
+
+
+
+# print the results
+# print(results.sample(5))
+
+# plot the results
+# colors = ['red', 'green']
+# cluster = ['Not Sarcastic', 'Sarcastic']
+# for i in range(num_clusters):
+#     plt.scatter(reduced_data[kmeans.labels_ == i, 0],
+#                 reduced_data[kmeans.labels_ == i, 1],
+#
+#                 s=20
+#                 )
+# plt.legend()
+# plt.show()
