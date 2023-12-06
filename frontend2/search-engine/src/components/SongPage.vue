f6b46146a2c2c1d67ccbaa8c2d0c5fed9072c80e
Michele Smaldone <michele.smaldone@usi.ch>
2023-12-06 01:10:21 +0100

    style finished.

--- frontend2/search-engine/src/components/SongPage.vue
+++ frontend2/search-engine/src/components/SongPage.vue
@@ -6,19 +6,40 @@ export default {
   data(){
     return {
       relevant_songs_list: [],
+      setSelectedSong: this.selectedSong,
+      loading: false,
+      isRelevant: true,
     }
   },
   props: {
     selectedSong: null
   },
-  async mounted() {
-    const res = await fetch(
-        "http://localhost:8000/relevant-documents/" + this.selectedSong.docid)
-    const relevant_songs = await res.json()
-    this.relevant_songs_list = relevant_songs;
+  mounted() {
+    this.fetchRelevant()
+  },
+  methods: {
+    getSongPage(song){
+      this.isRelevant = false;
+      this.setSelectedSong = song;
+      this.fetchRelevant();
+      this.isRelevant = true;
+    },
+    async fetchRelevant(){
+      try {
+        this.loading = true;
+        const res = await fetch(
+            "http://localhost:8000/relevant-documents/" + this.setSelectedSong.docid)
+        const relevant_songs = await res.json()
+        this.relevant_songs_list = relevant_songs;
+      } catch(err){
+        console.log(err);
+        this.relevant_songs_list = [];
+      } finally {
+        this.loading = false
+      }
 
-    // TODO: style
-  }
+    }
+  },
 }
 </script>
 
@@ -26,19 +47,29 @@ export default {
   <v-card
       class="mx-auto my-8 rounded-xl"
       max-width="700"
-      elevation="16">
+      elevation="16"
+      style="white-space: pre-wrap; text-align: center"
+  >
     <v-card-item>
-      <v-card-title>{{selectedSong.Title}}</v-card-title>
-      <v-card-subtitle>{{selectedSong.Artist}}</v-card-subtitle>
+      <v-card-title>{{setSelectedSong.Title}}</v-card-title>
+      <v-card-subtitle>{{setSelectedSong.Artist}}</v-card-subtitle>
     </v-card-item>
-    <v-card-text>{{selectedSong.Lyrics}}</v-card-text>
+    <v-card-text>{{setSelectedSong.Lyrics}}</v-card-text>
   </v-card>
-  <p>Related songs: </p>
-  <div v-if="relevant_songs_list.length">
-    <v-row>
-      <v-col v-for="song in relevant_songs_list" :key="song.id" cols="12" sm="3" md="3" lg="3">
-        <SongCard :song="song"></SongCard>
-      </v-col>
-    </v-row>
+  <div v-if="loading">
+    <h2 style="text-align: center" class="pa-4">Loading related songs...</h2>
   </div>
+  <div v-if="!loading && !relevant_songs_list.length">
+    <h2 style="text-align: center" class="pa-4">No related songs found</h2>
+  </div>
+  <div v-if="!loading && relevant_songs_list.length">
+      <h2 style="text-align: center">Related songs: </h2>
+      <v-row class="pa-4">
+        <v-col v-for="song in relevant_songs_list" :key="song.id" cols="12" sm="3" md="3" lg="3">
+          <SongCard :song="song" @songCardClicked="getSongPage" :is-relevant-list="isRelevant"></SongCard>
+        </v-col>
+      </v-row>
+    </div>
+
+
 </template>
